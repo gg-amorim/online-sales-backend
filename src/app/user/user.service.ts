557c53f96cfbdf0ff42a59e19bb1372cb5738bf4
Guilherme Amorim Gonçalez <guilherme.goncalez@w12.com.br>
2023-11-13 17:11:06 -0300

    feat: add user

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createUser.dto';
+import { UserEntity } from './interfaces/user.entity';
+import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(createUser: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
+    const passwordHashed = await hash(createUser.password, saltOrRounds);
+    const user = {
+      ...createUser,
+      password: passwordHashed,
+    };
+    return this.userRepository.save(user);
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
+  }
+}
